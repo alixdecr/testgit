286e7363fe643e4d77370c726e5d3d6a79a6751f
Alix Decrop <alix.decr@gmail.com>
2021-01-21 18:00:10 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+# 1. Create project folder
+# 2. Right click it, select Git Bash Here
+# 3. To create a file in the folder, use $ touch <file_name.extension>
+# 4. $ git init to initialise the .git folder
+# 5. Add name and email to the git configuration using $ git config --global user.name 'Alix Decrop'
+# and $ git config --global user.email 'alix.decr@gmail.com'
+# 6. To add file in the .git folder, use $ git add <file_name.extension>
+# 7. To remove file from the .git folder, use $ git rm --cached <file_name.extension>
+# 8. To add all project files, use $ git add .
+# 9. Use $ git status to see the status of .git
+# 10. Use $ git commit to commit
+
+def testFunction():
+    print('Hello Git!')
+    print('Updated...')
+
+testFunction()
